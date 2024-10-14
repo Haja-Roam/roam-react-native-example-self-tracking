a9a7894879f3b938743dd70750bda51433a23415
haja <hajakiyasudeen@gmail.com>
2024-10-14 19:19:58 +0530

    Create User

--- index.js
+++ index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Toast from 'react-native-simple-toast';
+import Roam from 'roam-reactnative';
+
+
+function registerListener(){
+    console.log('index register')
+    Roam.startListener('location', locations => {
+        Toast.show(JSON.stringify(locations), Toast.LONG);
+    })
+}
+
+AppRegistry.registerHeadlessTask('RoamHeadlessService', registerListener());
 
 AppRegistry.registerComponent(appName, () => App);
